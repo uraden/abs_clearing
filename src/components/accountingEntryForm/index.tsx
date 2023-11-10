d69f687989d66806ffa9f964e578cd0f4ca48a3b
Jamoliddin <jfahriddinov97@gmail.com>
2023-11-10 16:24:25 +0500

    added the form

---
+++ src/components/accountingEntryForm/index.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+import { Button, DatePicker, Form, Input, Select, Divider } from "antd";
+
+const { RangePicker } = DatePicker;
+const AccountingEntryForm: React.FC = () => {
+  return (
+    <>
+      <Form
+        labelCol={{ span: 4 }}
+        wrapperCol={{ span: 14 }}
+        layout="horizontal"
+        style={{border: "1px solid #808080", padding:"10px", borderRadius:"10px"}}
+      >
+        <Form layout="inline">
+          <Form.Item label="№ Док">
+            <Input  />
+          </Form.Item>
+          <Form.Item label="дата док / дата потверждение">
+            <RangePicker />
+          </Form.Item>
+        </Form>
+
+        <Divider />
+
+        <Form layout="inline">
+          <Form.Item label="Счет плательшика">
+            <Select style={{ width: "100px" }}>
+              <Select.Option value="demo">Demo</Select.Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item>
+            <Input />
+          </Form.Item>
+
+          <Form.Item label="ИНН/ПИНФЛ">
+            <Input />
+          </Form.Item>
+
+          <Form.Item>
+            <Input />
+          </Form.Item>
+        </Form>
+        <br />
+        <Form layout="inline">
+          <Form.Item label="Банк">
+            <Input />
+          </Form.Item>
+
+          <Form.Item label="Код Банка">
+            <Input />
+          </Form.Item>
+        </Form>
+
+        <Divider />
+
+        <h3> Сумма не указана</h3>
+
+        <Divider />
+
+        <Form layout="inline">
+          <Form.Item label="Счет плательшика">
+            <Select style={{ width: "100px" }}>
+              <Select.Option value="demo">Demo</Select.Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item>
+            <Input />
+          </Form.Item>
+
+          <Form.Item label="ИНН/ПИНФЛ">
+            <Input />
+          </Form.Item>
+
+          <Form.Item>
+            <Input />
+          </Form.Item>
+        </Form>
+        <br />
+        <Form layout="inline">
+          <Form.Item label="Банк">
+            <Input />
+          </Form.Item>
+
+          <Form.Item label="Код Банка">
+            <Input />
+          </Form.Item>
+        </Form>
+
+        <Divider />
+
+        <Form layout="inline">
+          <Form.Item label="Детали платежа">
+            <h4 style={{ margin: "0px 0px " }}>Some data</h4>
+          </Form.Item>
+
+          <Form.Item label="платеж">
+            <Select style={{ width: "100px" }}>
+              <Select.Option value="demo">Demo</Select.Option>
+            </Select>
+          </Form.Item>
+        </Form>
+
+        <Divider />
+
+        <Form layout="inline">
+          <Form.Item>
+            <Button>Сократить</Button>
+          </Form.Item>
+
+          <Form.Item>
+            <Button>Загрузить</Button>
+          </Form.Item>
+        </Form>
+      </Form>
+    </>
+  );
+};
+
+export default () => <AccountingEntryForm />;
--- src/pages/mainPage/index.tsx
+++ src/pages/mainPage/index.tsx
@@ -1,10 +1,13 @@
 import SimpleTable from "../../components/simpleTable";
+import AccountingEntryForm from "../../components/accountingEntryForm"
 
 
 const Main = () => {
   return (
     <>
       <SimpleTable />
+       <h3>Форма внизу</h3>
+      <AccountingEntryForm />
     </>
   );
 };
