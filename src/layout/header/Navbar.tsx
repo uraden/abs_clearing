f9e7306fa2caad1fbf00e0e204b28517652b3042
Jamoliddin <jfahriddinov97@gmail.com>
2023-11-14 15:34:06 +0500

    navbar

--- src/layout/header/Navbar.tsx
+++ src/layout/header/Navbar.tsx
@@ -1,3 +1,100 @@
+import React, {ReactNode, useState} from 'react';
+import {
+  TeamOutlined,
+  UserOutlined,
+  PieChartOutlined,
+  DesktopOutlined,
+  FileOutlined
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Layout, Menu, theme } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const Navbar: React.FC = ({ children }: { children: ReactNode }) => {
+
+  const [collapsed, setCollapsed] = useState(false);
+  const [men, setMen] = useState(false);
+  const [marginLeft, setMarginLeft] = useState(false)
+  const [withLayout, setWithLayout] = useState(false)
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  type MenuItem = Required<MenuProps>['items'][number];
+
+  function getItem(
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+  ): MenuItem {
+    return {
+      key,
+      icon,
+      children,
+      label,
+    } as MenuItem;
+  }
+
+  const items: MenuItem[] = [
+    getItem('Option 1', '1', <PieChartOutlined />),
+    getItem('Option 2', '2', <DesktopOutlined />),
+    getItem('User', 'sub1', <UserOutlined />, [
+      getItem('Tom', '3'),
+      getItem('Bill', '4'),
+      getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined />),
+  ];
+
+
+  return (
+    <Layout hasSider>
+      <Sider
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          left: 0,
+          top: 0,
+          bottom: 0,
+          zIndex: 1
+        }}
+        collapsible collapsed={collapsed} onCollapse={(value) =>{
+          setCollapsed(value)
+          setMen(!men)
+          console.log(`here ${!men}`)
+          setMarginLeft(!marginLeft)
+          setWithLayout(!withLayout)
+          console.log('marginLeft', marginLeft)
+        }}
+      >
+         <div className="demo-logo-vertical" />
+         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
+      <Layout className="site-layout" style={{ marginLeft: marginLeft ? 88 : 200,  minHeight: '100vh', width: withLayout ? '94vw' : '88vw' }}>
+        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+          <div style={{ padding: 24, textAlign: 'center', background: colorBgContainer, zIndex: "99" }}>
+            {children}
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default Navbar;
+
+
+
+
+
+
+
 // import React, { useState, ReactNode } from 'react';
 // import {
 //   DesktopOutlined,
@@ -45,134 +142,64 @@
 //     token: { colorBgContainer },
 //   } = theme.useToken();
 
+//   type MenuItem = Required<MenuProps>['items'][number];
+
+//     function getItem(
+//     label: React.ReactNode,
+//     key: React.Key,
+//     icon?: React.ReactNode,
+//     children?: MenuItem[],
+//   ): MenuItem {
+//     return {
+//       key,
+//       icon,
+//       children,
+//       label,
+//     } as MenuItem;
+//   }
+
+
+//     const items: MenuItem[] = [
+//     getItem('Option 1', '1', <PieChartOutlined />),
+//     getItem('Option 2', '2', <DesktopOutlined />),
+//     getItem('User', 'sub1', <UserOutlined />, [
+//       getItem('Tom', '3'),
+//       getItem('Bill', '4'),
+//       getItem('Alex', '5'),
+//     ]),
+//     getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+//     getItem('Files', '9', <FileOutlined />),
+//   ];
+
 //   return (
-//     <Layout style={{ minHeight: '100vh', width: '99vw' }}>
-//       <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+//     <Layout hasSider>
+//       <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)} style={{
+//           overflow: 'auto',
+//           height: '100vh',
+//           position: 'fixed',
+//           left: 0,
+//           top: 0,
+//           bottom: 0,
+//           zIndex: 1
+//         }}>
 //         <div className="demo-logo-vertical" />
 //         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
 //       </Sider>
-//       <Layout>        
+//       <Layout>
+//         <Header style={{ padding: 0, background: colorBgContainer }} />
 //         <Content style={{ margin: '0 16px' }}>
 //           <Breadcrumb style={{ margin: '16px 0' }}>
 //             <Breadcrumb.Item>User</Breadcrumb.Item>
 //             <Breadcrumb.Item>Bill</Breadcrumb.Item>
 //           </Breadcrumb>
 //           <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>
-//            {children}
-//            {/* <h2>fdsfsafsaf</h2> */}
+//             {children}
 //           </div>
 //         </Content>
-//         <Footer style={{ textAlign: 'center'}}>Ant Design ©2023 Created by Ant UED</Footer>
+//         <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
 //       </Layout>
 //     </Layout>
 //   );
 // };
 
 // export default Navbar;
-
-
-
-import React, {ReactNode, useState} from 'react';
-import {
-  AppstoreOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  ShopOutlined,
-  TeamOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-  PieChartOutlined,
-  DesktopOutlined,
-  FileOutlined
-} from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Layout, Menu, theme } from 'antd';
-
-const { Header, Content, Footer, Sider } = Layout;
-
-const items: MenuProps['items'] = [
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  AppstoreOutlined,
-  TeamOutlined,
-  ShopOutlined,
-].map((icon, index) => ({
-  key: String(index + 1),
-  icon: React.createElement(icon),
-  label: `nav ${index + 1}`,
-}));
-
-const Navbar: React.FC = ({ children }: { children: ReactNode }) => {
-
-  const [collapsed, setCollapsed] = useState(false);
-
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
-
-  type MenuItem = Required<MenuProps>['items'][number];
-
-  function getItem(
-    label: React.ReactNode,
-    key: React.Key,
-    icon?: React.ReactNode,
-    children?: MenuItem[],
-  ): MenuItem {
-    return {
-      key,
-      icon,
-      children,
-      label,
-    } as MenuItem;
-  }
-
-  const items: MenuItem[] = [
-    getItem('Option 1', '1', <PieChartOutlined />),
-    getItem('Option 2', '2', <DesktopOutlined />),
-    getItem('User', 'sub1', <UserOutlined />, [
-      getItem('Tom', '3'),
-      getItem('Bill', '4'),
-      getItem('Alex', '5'),
-    ]),
-    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-    getItem('Files', '9', <FileOutlined />),
-  ];
-
-
-  return (
-    <Layout hasSider>
-      <Sider
-        style={{
-          overflow: 'auto',
-          height: '100vh',
-          position: 'fixed',
-          left: 0,
-          top: 0,
-          bottom: 0,
-          zIndex: 1
-        }}
-
-        collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}
-      >
-         <div className="demo-logo-vertical" />
-         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
-      </Sider>
-      <Layout className="site-layout" style={{ marginLeft: 200,  minHeight: '100vh', width: '88vw'  }}>
-        <Header style={{ padding: 0, background: colorBgContainer }} />
-        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-          <div style={{ padding: 24, textAlign: 'center', background: colorBgContainer, zIndex: "99" }}>
-            {children}
-           
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
-      </Layout>
-    </Layout>
-  );
-};
-
-export default Navbar;
