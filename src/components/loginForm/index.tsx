6b3ac17e6c927e3bd984f64079b6f2d09218db5f
Bexzod Shaymonov <Shaymonov@valuta.com>
2023-11-20 15:12:51 +0500

    Add antdesign notification

--- src/components/loginForm/index.tsx
+++ src/components/loginForm/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { Button, Form, Input } from "antd";
+import React, { useState, useEffect } from "react";
+import { Button, Form, Input, notification } from "antd";
 import { login } from "./request";
+import { useNavigate } from "react-router-dom";
 
 const Roles = [
   {
@@ -13,75 +14,93 @@ const Roles = [
   },
 ];
 
-const onFinish = async (values: unknown) => {
-  console.log("Success:", values);
-  const response = await login(values);
-  console.log("responsett: ", response);
-  if (response && response.status === 401) {
-    // toast.error(response.title);
-    // setMessage(response.title);
-    setTimeout(() => {
-      // setMessage("");
-    }, 4000);
-  }
-  if (response && response.token) {
-    localStorage.setItem("token", response.token);
-  }
-};
-
-const onFinishFailed = (errorInfo: unknown) => {
-  console.log("Failed:", errorInfo);
-};
-
 type FieldType = {
   login?: string;
   password?: string;
-  remember?: string;
 };
 
-const LoginForm: React.FC = () => (
-  <div>
-    <h3 style={{ marginBottom: 4, fontSize: "54px", color: "white" }}>
-      ABS Login
-    </h3>
-    <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      style={{ width: 700 }}
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-      layout="vertical"
-    >
-      <Form.Item<FieldType>
-        label="Username"
-        name="login"
-        rules={[{ required: true, message: "Please input your username!" }]}
-      >
-        <Input />
-      </Form.Item>
+const LoginForm: React.FC = () => {
+  const [api, contextHolder] = notification.useNotification();
+  const [isLoading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-      <Form.Item<FieldType>
-        label={<label style={{ color: "white" }}>Password</label>}
-        name="password"
-        rules={[{ required: true, message: "Please input your password!" }]}
-      >
-        <Input.Password />
-      </Form.Item>
+  useEffect(() => {
+    localStorage.removeItem('token');
+  }, []);
 
-      <Button
-        type="primary"
-        htmlType="submit"
-        size="middle"
-        block
-        style={{ outline: "none", width: "466px", backgroundColor: "#6C63FF" }}
+  const onFinish = async (values: unknown) => {
+    setLoading(true);
+    const response = await login(values);
+    console.log("responsett: ", response);
+    if (response && response.status === 401) {
+      api.error({
+        message: response.detail,
+        description: response.title,
+        duration: 4,
+      });
+    }
+
+    if (response && response.token) {
+      localStorage.setItem("token", response.token);
+      navigate("/");
+    }
+    setLoading(false);
+  };
+
+  const onFinishFailed = (errorInfo: unknown) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <div>
+      {contextHolder}
+      <h3 style={{ marginBottom: 4, fontSize: "54px", color: "white" }}>
+        ABS Login
+      </h3>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ width: 700 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        layout="vertical"
       >
-        Log-in
-      </Button>
-    </Form>
-  </div>
-);
+        <Form.Item<FieldType>
+          label="Username"
+          name="login"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          label={<label style={{ color: "white" }}>Password</label>}
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Button
+          type="primary"
+          htmlType="submit"
+          size="middle"
+          loading={isLoading}
+          block
+          style={{
+            outline: "none",
+            width: "466px",
+            backgroundColor: "#6C63FF",
+          }}
+        >
+          Log-in
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
 export default LoginForm;
