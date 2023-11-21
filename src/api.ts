63e759ceff924e52d6b147fefc9ed69233498dd1
Bexzod Shaymonov <Shaymonov@valuta.com>
2023-11-21 10:38:04 +0500

    Add request without token

--- src/api.ts
+++ src/api.ts
@@ -2,5 +2,6 @@ const baseUrl = 'http://100.1.0.52:8081/v1/api';
 
 export default {
   login: () => [baseUrl, 'auth', 'authenticate'].join('/'),
-  orders: () => [baseUrl, 'orders'].join('/'),
+  // orders: () => [baseUrl, 'orders'].join('/'),
+  orders: () => [baseUrl, 'auth', 'orders'].join('/'),
 }
--- src/pages/accountList/request.tsx
+++ src/pages/accountList/request.tsx
@@ -1,9 +1,10 @@
 import { httpClient } from "../../httpClient";
 import api from '../../api';
+import axios from "axios";
 
 export const getAccountList = async () => {
   try {
-    const request = await httpClient.post(api.orders());
+    const request = await axios.post(api.orders());
     console.log('request: ', request);
   } catch (error) {
     console.log('err: ', error);
