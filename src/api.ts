e2cdaee25395b574c5c807731b558eb142b73ffd
Bexzod Shaymonov <Shaymonov@valuta.com>
2023-11-22 12:23:25 +0500

    Change order route

--- src/api.ts
+++ src/api.ts
@@ -2,6 +2,6 @@ const baseUrl = import.meta.env.VITE_BASEURL;
 
 export default {
   login: () => [baseUrl, 'auth', 'authenticate'].join('/'),
-  // orders: () => [baseUrl, 'orders'].join('/'),
-  orders: () => [baseUrl, 'auth', 'orders'].join('/'),
+  orders: () => [baseUrl, 'orders'].join('/'),
+  // orders: () => [baseUrl, 'auth', 'orders'].join('/'),
 }
--- src/pages/accountList/request.tsx
+++ src/pages/accountList/request.tsx
@@ -1,13 +1,14 @@
 // import { httpClient } from "../../httpClient";
-import api from '../../api';
+import api from "../../api";
 import axios from "axios";
+import { httpClient } from "../../httpClient";
 
 export const getAccountList = async () => {
   try {
-    const request = await axios.post(api.orders());
+    const request = await httpClient.post(api.orders());
     console.log('request: ', request);
     return request;
   } catch (error) {
-    console.log('err: ', error);
+    console.log("err: ", error);
   }
 };
