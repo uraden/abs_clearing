46b237b0e127a5607ebe827f537e0c4e71ef09e4
Jamoliddin <jfahriddinov97@gmail.com>
2024-01-08 18:11:00 +0500

    new change

--- src/pages/accountFormNew/index.tsx
+++
@@ -1,7 +0,0 @@
-import AccountingEntryFormNew from "../../components/accountingEntryFormNew";
-
-const AccountFormNew = () => {
-  return <AccountingEntryFormNew />;
-};
-
-export default AccountFormNew;
